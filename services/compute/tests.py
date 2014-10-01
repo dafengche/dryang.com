11c90952a4e5c01c206cad1c29aecb44db0bfa9b
Xiaobo Yang <maxy@web01.dryang.com>
2014-10-01 11:23:10 +0100

    Unit test case created for service compute

---
+++ services/compute/tests.py
@@ -0,0 +1,25 @@
+import unittest
+import compute
+
+class TestCompute(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_add(self):
+        self.assertEqual(compute.add(1, 2), 3)
+
+    def test_sub(self):
+        self.assertEqual(compute.sub(1, 2), -1)
+
+    def test_mul(self):
+        self.assertEqual(compute.mul(2, 3), 6)
+
+    def test_div(self):
+        self.assertEqual(compute.div(6, 2), 3)
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
