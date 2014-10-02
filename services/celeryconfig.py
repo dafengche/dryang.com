e31fa17dfc2abbff5ac9e3e87a8ef5a18e3b8fa3
celery <celery@mw01.dryang.com>
2014-10-02 22:49:19 +0100

    Experimenting celery config...

--- services/celeryconfig.py
+++ services/celeryconfig.py
@@ -3,3 +3,22 @@ CELERY_RESULT_BACKEND = 'redis://'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
+'''
+# Routing config
+from kombu import Exchange, Queue
+# Rename the default queue from 'celery' to 'default'
+CELERY_QUEUES = (
+    Queue('default', Exchange('default'), routing_key = 'default'),
+    Queue('plot', Exchange('plot'), routing_key = 'plot'),
+)
+CELERY_DEFAULT_QUEUE = 'default'
+CELERY_DEFAULT_EXCHANGE_TYPE = 'direct'
+CELERY_DEFAULT_ROUTING_KEY = 'default'
+# Route tasks to queues
+CELERY_ROUTES = {
+    'plot.plot.make_plot_mpl': {
+        'queue': 'plot',
+        'routing_key': 'plot'
+    },
+}
+'''
