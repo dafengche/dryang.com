c4831016db1fea26895e537f279749a7a8b94ba2
Xiaobo Yang <xiaobo.yang@gmail.com>
2014-11-12 00:12:05 +0000

    Working on UI for badminton organisers...

--- badminton/views.py
+++ badminton/views.py
@@ -19,32 +19,55 @@ def is_user_in_group_badminton_player(user):
 def is_user_in_group_badminton_organiser(user):
     return is_user_in_group(user, 'badminton_organiser')
 
-def get_total_play_count(year):
-    total_play_count = 0
+def get_play_count(year):
+    play_count = 0
     games = Game.objects.filter(play_date__year = year)
-    for g in games: total_play_count += len(g.players.all())
-    return total_play_count
+    for g in games: play_count += len(g.players.all())
+    return play_count
 
-def get_total_bal(year):
-    # Calculate the total balance (contributions - costs)
-    total_bal = 0.
+def get_bal(year):
+    # Calculate the balance (contributions - costs)
+    bal = 0.
     contribs = Contribution.objects.filter(financial_year = year)
-    for c in contribs: total_bal += c.amount
+    for c in contribs: bal += c.amount
     costs = Cost.objects.filter(financial_year = year)
-    for c in costs: total_bal -= c.amount
-    return total_bal
+    for c in costs: bal -= c.amount
+    return bal
+
+def get_players(year):
+    games = Game.objects.filter(play_date__year = year)
+    if len(games) == 0: return None
+
+    player_count = {}
+    player_name = {}
+    for g in games:
+        players = g.players.all()
+        for p in players:
+            if p.username in player_count: player_count[p.username] += 1
+            else:
+                player_count[p.username] = 1
+                player_name[p.username] = p.first_name + ' ' + p.last_name
+
+    player_bal = {}
+    cost_per_play = get_bal(year) / get_play_count(year)
+    for (k, v) in player_count.items():
+        player_bal[player_name[k]] = cost_per_play * v
+        contribs = Contribution.objects.filter(financial_year = year).filter(contributor__username = k)
+        for c in contribs: player_bal[player_name[k]] += c.amount
+    return sorted(player_bal.iteritems())
 
 def get_user_bal_and_games(year, username):
-    total_bal = get_total_bal(year)
-    total_play_count = get_total_play_count(year)
+    bal = get_bal(year)
+    play_count = get_play_count(year)
+    if play_count == 0: return (0., None)
 
     # Calculate user' balance
-    bal = 0.
+    user_bal = 0.
     contribs = Contribution.objects.filter(financial_year = year).filter(contributor__username = username)
-    for c in contribs: bal += c.amount
+    for c in contribs: user_bal += c.amount
     games = Game.objects.filter(play_date__year = year).filter(players__username = username)
-    if len(games) > 0: bal += total_bal / total_play_count * len(games)
-    return (bal, games)
+    if len(games) > 0: user_bal += bal / play_count * len(games)
+    return (user_bal, games)
 
 def index(request):
     data = {'title': 'Friday badminton'}
@@ -80,16 +103,29 @@ def list(request):
 @login_required(login_url = reverse_lazy('dryang-auth:login'))
 @user_passes_test(is_user_in_group_badminton_organiser, login_url = '/auth/access-denied/')
 def list_all(request):
+    param = request.GET.get('p', None)
+    if not param: param = 'game'
+    logger.debug('p=' + param)
     year = request.GET.get('year', None)
     if not year: year = date.today().year
     else:
-        logger.debug("Found parameter 'year' in request")
         year = int(year)
     logger.debug('Year: %d' % year)
 
     data = {'title': 'Friday badminton',
+            'p': param,
             'year': year,
             'current_year': date.today().year,
-            'bal': get_total_bal(year),
-            'total_play_count': get_total_play_count(year)}
+            'bal': get_bal(year),
+            'play_count': get_play_count(year)}
+
+    if 'game' == param:
+        data['games'] = Game.objects.filter(play_date__year = year)
+    elif 'player' == param:
+        data['players'] = get_players(year)
+    elif 'cost' == param:
+        data['costs'] = Cost.objects.filter(financial_year = year)
+    elif 'contrib' == param:
+        data['contribs'] = Contribution.objects.filter(financial_year = year)
+
     return render(request, 'badminton/list-all.html', data)
