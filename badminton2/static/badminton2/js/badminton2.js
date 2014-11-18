f6e1a51ceac915028c94b5f3ebe4670d08c62701
Xiaobo Yang <xiaobo.yang@gmail.com>
2014-11-18 23:13:13 +0000

    Flot introducted for client side visualisation

--- badminton2/static/badminton2/js/badminton2.js
+++ badminton2/static/badminton2/js/badminton2.js
@@ -25,6 +25,21 @@ $(function() {
   $('#badminton2_record').on('change', function() {
     var r = $(this).val();
     var params = {'r': r, 'y': 2014};
+    getData(params);
+  });
+
+  // Init
+  init();
+
+  function init() {
+    getData({'r': $('#badminton2_record').val(), 'y': new Date().getFullYear()});
+  }
+
+  function getData(params) {
+    console.log(params);
+    var msg = $('#badminton2_msg');
+    var container = $('#badminton2_plot');
+    var r = $('#badminton2_record').val();
     $.ajax({
       url: 'get-data/',
       type: 'post',
@@ -37,6 +52,37 @@ $(function() {
           return;
         }
         console.log(result);
+        var data = [];
+        if (r === 'g') { // Games
+          $.each(result['data'], function(k, v) {
+            data.push([new Date(v['date']).getTime(), v['players'].length]);
+          });
+//          console.log(data);
+          $.plot(container,
+            [{
+              data  : data,
+              color : '#0062E3',
+              points: {fillColor: '#0062E3', show: true},
+              lines : {show: true}
+            }],
+            {
+              xaxis: {
+                mode: 'time'
+              },
+              grid: {
+                hoverable        : true,
+                borderWidth      : 3,
+                mouseActiveRadius: 50,
+                backgroundColor  : {colors: ['#FFFFFF', '#EDF5FF']},
+                axisMargin       : 20
+              }
+            }
+          );
+        } else if (r === 'p') { // Players
+        } else if (r === 'c') { // Costs
+        } else if (r === 'ctb') { // Contributions
+        } else if (r === 'b') { // My balance
+        }
       },
       error: function(result) {
 //        container.zoomableImage('update', STATIC_URL + 'common/images/failed.jpg');
@@ -46,6 +92,6 @@ $(function() {
 //        $('#verif_plot_btn').removeAttr('disabled');
       }
     });
-  });
+  }
 
 });
